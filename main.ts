452145c1a54f2bd458aab819191bc76ae951f19b
Denis U <denisudovcic@gmail.com>
2024-10-17 17:55:43 +0100

    sort

--- main.ts
+++ main.ts
@@ -19,11 +19,10 @@ function main() {
   // console.log("args", process.argv[2]);
 
   fs.readFile(fileArg, { encoding: "utf8" }, (err, data) => {
-    const plans = JSON.parse(data);
+    const plans = JSON.parse(data) as Plan[];
 
     rl.on("line", (line) => {
       // const plans = JSON.parse(fs.readFileSync(fileArg, "utf-8"));
-      console.log("line", line);
       if (line === "exit") {
         rl.close();
         return;
@@ -33,14 +32,24 @@ function main() {
         console.log("Incorrect input");
       } else {
         try {
-          plans.forEach((plan) => {
+          const prices = plans.map((plan: Plan) => {
             const calculator = new Planner(plan);
             const annualPrice = calculator.calculateAnnualPriceFromUsage(
               Number(lineArgs[1])
             );
-
-            console.log(`${plan.supplier},${plan.plan},${annualPrice}`);
+            return {
+              supplier: plan.supplier,
+              plan: plan.plan,
+              price: annualPrice,
+            };
           });
+          prices.sort((a, b) => a.price - b.price);
+
+          prices.forEach((planPrice) =>
+            console.log(
+              `${planPrice.supplier},${planPrice.plan},${planPrice.price.toFixed(2)}`
+            )
+          );
         } catch {
           throw new Error("No plans").message;
         }
--- planner.ts
+++ planner.ts
@@ -35,6 +35,6 @@ export class Planner {
 
     priceTotalPounds = priceTotalPounds * this.VAT;
 
-    return priceTotalPounds.toFixed(2);
+    return priceTotalPounds;
   }
 }
